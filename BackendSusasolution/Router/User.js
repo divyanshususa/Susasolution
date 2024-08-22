70e9ba98bed6a981061ae2bfb11be893bf9eb1c5
Your Name <you@example.com>
2024-08-22 13:28:18 +0530

    backend updated

--- BackendSusasolution/Router/User.js
+++ BackendSusasolution/Router/User.js
@@ -3,17 +3,11 @@ const router = express.Router();
 const userController = require("../Controller/User");
 const multer = require("multer");
 const upload = multer({ dest: "./uploads/" });
-const User = require("../models/Signup");
 const fs = require("fs");
 const path = require("path");
 const mongoose = require("mongoose");
 
-// router.post("/create", upload.fields([
-//   { name: "panCardImage", maxCount: 1 },
-//   { name: "userRole" },
-//   { name: "email" },
-//   { name: "password" },
-// ]), userController.createUser);
+
 
 router.post(
   "/create",
