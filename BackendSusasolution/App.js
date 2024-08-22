bb4cfd294451ab2c01d28e5b39e02d8d97601a90
Your Name <you@example.com>
2024-08-22 17:25:35 +0530

    backend api

--- BackendSusasolution/App.js
+++ BackendSusasolution/App.js
@@ -21,7 +21,9 @@ connectDB();
 // app.use("/auth",userroutes)
 app.use("/signup", userroutes);
 app.use("/api/projects", projectRoute);
-
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}` );
