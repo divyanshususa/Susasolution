2ce99d44e37bc39f8555fd2aced0e8d904398973
Your Name <you@example.com>
2024-08-22 17:16:18 +0530

    backend updated

--- BackendSusasolution/App.js
+++ BackendSusasolution/App.js
@@ -6,7 +6,7 @@ const connectDB = require("./config");
 const userController = require("./Controller/User");
 const projectRoute = require("./Router/project");
 const userroutes = require('./Router/User');
-
+const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.urlencoded({ extended: true })); 
@@ -23,7 +23,7 @@ app.use("/signup", userroutes);
 app.use("/api/projects", projectRoute);
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
  
